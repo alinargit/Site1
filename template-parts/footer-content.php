6c794ee8a934779e3573cce1fb1b61d966dd996f
alinargit <127021881+alinargit@users.noreply.github.com>
2025-05-29 18:16:18 +0200

    Initial commit

---
+++ template-parts/footer-content.php
@@ -0,0 +1,5 @@
+<footer class="u-align-center u-clearfix u-container-align-center u-footer u-grey-80 u-footer" id="footer">
+  <div class="u-clearfix u-sheet u-sheet-1">
+    <p class="u-small-text u-text u-text-variant u-text-1">Copyright - DS</p>
+  </div>
+</footer>
